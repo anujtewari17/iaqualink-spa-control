2b9f1033c30d972aa3d90f80e55f8259e5ab5922
anujtewari17 <46807944+anujtewari17@users.noreply.github.com>
2025-07-13 23:42:46 -0700

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 // iAquaLink 2.0 Simplified Spa Control Proxy
 // Only handles spa mode, spa heat, filter pump, and temperature
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   // Set CORS headers
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
